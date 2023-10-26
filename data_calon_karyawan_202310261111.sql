474da4a99fbee71edfd167b28e88252a2602ea96
kumil14022 <133571166+kumil14022@users.noreply.github.com>
2023-10-26 11:46:02 -0700

    first commit

---
+++ data_calon_karyawan_202310261111.sql
@@ -0,0 +1,11 @@
+INSERT INTO public.data_calon_karyawan (nama,umur,jenis_kelamin,status_pernikahan,pengalaman,pengetahuan) VALUES
+	 ('John Doe',35,'Laki-laki','Menikah','YA','YA'),
+	 ('Jane Smith',28,'Perempuan','Lajang','TIDAK','YA'),
+	 ('Michael Johnson',42,'Laki-laki','Menikah','TIDAK','YA'),
+	 ('Sarah Lee',30,'Perempuan','Lajang','YA','TIDAK'),
+	 ('David Wang',38,'Laki-laki','Menikah','YA','TIDAK'),
+	 ('Emily Chen',25,'Perempuan','Lajang','YA','YA'),
+	 ('Brian Tan',45,'Laki-laki','Menikah','TIDAK','YA'),
+	 ('Lisa Kim',32,'Perempuan','Lajang','TIDAK','YA'),
+	 ('Matthew Wong',40,'Laki-laki','Menikah','TIDAK','TIDAK'),
+	 ('Olivia Ng',27,'Perempuan','Lajang','YA','YA');
